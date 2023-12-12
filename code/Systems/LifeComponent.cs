d19c29b942e1b9d169da5d18c7ef70d7e85a6d1d
Xenthio <thegooster88@gmail.com>
2023-12-13 10:25:06 +1100

    Lifestuiff

---
+++ code/Systems/LifeComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPSKit;
+
+public class LifeComponent : Component
+{
+	[Property] public float Health { get; set; } = 100.0f;
+
+	[Property] public LifeState LifeState { get; set; } = LifeState.Alive;
+
+	public Action<float,string[]> OnTakeDamage;
+	public Action OnKilled;
+
+	public void TakeDamage( float damage, string[] tags)
+	{
+		Health -= damage;
+		if (LifeState == LifeState.Alive && Health <= 0) Kill();
+		OnTakeDamage(damage, tags);
+	}
+
+	public void Kill()
+	{
+		LifeState = LifeState.Dead;
+		OnKilled();
+	}
+}
+
+public enum LifeState
+{
+	Alive,
+	Dead,
+	Respawning,
+	Limbo
+}
