987a5c277632949a76eb53ec4ed09b87b1b95b8e
Xenthio <thegooster88@gmail.com>
2023-12-15 20:09:35 +1100

    Update Game.cs

--- code/Game.cs
+++ code/Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Sandbox;
 
 namespace FPSKit;
+// delete this
 public class GameController : GameObject
 { 
 	public GameController()
