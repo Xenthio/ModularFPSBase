1255a93764da85f53d12040cd6c1c10f788dfe0e
Xenthio <thegooster88@gmail.com>
2023-12-19 13:00:07 +1100

    cleanup more

--- code/Game/Game.cs
+++ code/Game/Game.cs
@@ -1,5 +1,7 @@
-﻿global using System;
+global using System;
 global using Sandbox;
 global using System.Linq;
 global using System.Threading.Tasks;
 global using System.Collections.Generic;
+
+namespace FPSKit;
