93b5bb91585623d937040f678b6dac5d97d7df7a
Xenthio <thegooster88@gmail.com>
2023-12-19 12:11:10 +1100

    Update BulletSystem.cs

--- code/Systems/BulletSystem.cs
+++ code/Systems/BulletSystem.cs
@@ -38,12 +38,6 @@ public partial class Bullet
 
 			forward += Vector3.Random * info.Spread;
 
-			// check if we started in something penetrable
-			var trpcheck = GameManager.ActiveScene.Trace.Ray( position, position + (forward * 1) ).WithAnyTags( "penetrable", "water" ).Run();
-			if ( trpcheck.StartedSolid )
-			{
-				info.IgnoreObject = trpcheck.GameObject;
-			}
 			var tr = GameManager.ActiveScene.Trace.Ray( position, position + (forward * 10000) )
 				.UseHitboxes()
 				//.Ignore( info.Owner )
@@ -52,9 +46,6 @@ public partial class Bullet
 				.WithoutTags( "trigger", "skybox", "playerclip" )
 				.Run();
 
-			var tracerstart = info.TracerPosition ?? tr.StartPosition;
-			CreateTracerEffect( info, tracerstart, tr.EndPosition );
-			DoBulletFlyby( info.Owner, tr.StartPosition, tr.EndPosition, tr.Direction );
 			if ( tr.Hit )
 			{
 				//tr.Surface.DoBulletImpact( tr );
@@ -74,41 +65,12 @@ public partial class Bullet
 				if (tr.GameObject.Components.TryGet<LifeComponent>(out var life))
 				{
 					life.TakeDamage( dmgInfo );
-				} else if ( tr.GameObject.Components.TryGet<Rigidbody>( out var rigidbody ) )
-				{
-					rigidbody.ApplyImpulseAt( info.Position, info.Direction );
-				}
+				} 
 
 				if ( info.OnDealDamage != null ) info.OnDealDamage( dmgInfo );
 				if ( info.OnBulletHit != null ) info.OnBulletHit( tr );
 
-
-				//if ( ((tr.Entity.Tags.Has( "penetrable" ) || tr.Entity.Tags.Has( "water" )) || tr.Entity is ShatterGlass || tr.Entity is GlassShard) && level < 16 )
-				//{
-				//	var newinfo = info;
-				//	newinfo.IgnoreEntity = tr.Entity;
-				//	newinfo.Position = tr.HitPosition;
-				//	newinfo.Direction = forward;
-				//	newinfo.Spread = 0;
-				//	ShootBullet( newinfo, level + 1 );
-				//}
 			}
 		}
-	}
-	public static void CreateTracerEffect( BulletInfo info, Vector3 StartPosition, Vector3 EndPosition )
-	{
-		//var system = Particles.Create( info.TracerOverride ?? "particles/tracers/tracer.generic.vpcf" );
-		//system?.SetPosition( 0, StartPosition );
-		//system?.SetPosition( 1, EndPosition );
-	}
-
-	public static void DoBulletFlyby( GameObject Owner, Vector3 StartPosition, Vector3 EndPosition, Vector3 Direction )
-	{
-		if ( Owner != null && !Owner.IsProxy ) return;
-
-		var posforsound = StartPosition + Direction * Vector3.Dot( Camera.Position - StartPosition, Direction );
-		posforsound = posforsound.Clamp( Vector3.Min( StartPosition + (Direction * 70), EndPosition ), Vector3.Max( StartPosition + (Direction * 70), EndPosition ) );
-
-		//Sound.FromWorld( "flyby.pistol", posforsound );
 	}
 }
