e6536626a97f3dfd9d67646ff657a41c71f997f1
Xenthio <thegooster88@gmail.com>
2023-12-22 07:56:09 +1100

    Headshot Multiplier

--- code/Inventory/Items/GunComponent.cs
+++ code/Inventory/Items/GunComponent.cs
@@ -6,6 +6,7 @@ public class GunComponent : WeaponComponent
 	[Property, Group( "Primary Attack" )] public float PrimarySpread { get; set; } = 0.024f;
 	[Property, Group( "Primary Attack" )] public float PrimaryRecoil { get; set; } = 1.0f;
 	[Property, Group( "Primary Attack" )] public float PrimaryForce { get; set; } = 10.0f;
+	[Property, Group( "Primary Attack" )] public float PrimaryHeadshotMultiplier { get; set; } = 2.0f;
 	[Property, Group( "Primary Attack" )] public int PrimaryBulletCount { get; set; } = 1;
 	[Property, Group( "Primary Attack" )] public SoundEvent PrimaryShootSound { get; set; }
 	[Property, Group( "Primary Attack" )] public ParticleSystem PrimaryMuzzleflash { get; set; }
@@ -16,7 +17,8 @@ public class GunComponent : WeaponComponent
 	[Property, Group( "Secondary Attack" )] public float SecondarySpread { get; set; } = 0.024f;
 	[Property, Group( "Secondary Attack" )] public float SecondaryRecoil { get; set; } = 1.0f;
 	[Property, Group( "Secondary Attack" )] public float SecondaryForce { get; set; } = 10.0f;
-	[Property, Group( "Primary Attack" )] public int SecondaryBulletCount { get; set; } = 1;
+	[Property, Group( "Secondary Attack" )] public float SecondaryHeadshotMultiplier { get; set; } = 2.0f;
+	[Property, Group( "Secondary Attack" )] public int SecondaryBulletCount { get; set; } = 1;
 	[Property, Group( "Secondary Attack" )] public SoundEvent SecondaryShootSound { get; set; }
 	[Property, Group( "Secondary Attack" )] public ParticleSystem SecondaryMuzzleflash { get; set; }
 	public override void PrimaryAttack()
@@ -29,6 +31,7 @@ public class GunComponent : WeaponComponent
 			Position = OwnerInventory.Eye.Transform.Position,
 			Direction = OwnerInventory.Eye.Transform.Rotation.Forward,
 			Force = PrimaryForce,
+			HeadshotMultiplier = PrimaryHeadshotMultiplier,
 			Count = PrimaryBulletCount,
 		};
 		Sound.Play( PrimaryShootSound, OwnerInventory.Eye.Transform.Position );
@@ -47,6 +50,7 @@ public class GunComponent : WeaponComponent
 			Position = OwnerInventory.Eye.Transform.Position,
 			Direction = OwnerInventory.Eye.Transform.Rotation.Forward,
 			Force = SecondaryForce,
+			HeadshotMultiplier = SecondaryHeadshotMultiplier,
 			Count = SecondaryBulletCount,
 		};
 		Sound.Play( SecondaryShootSound, OwnerInventory.Eye.Transform.Position );
