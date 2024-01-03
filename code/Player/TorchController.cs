c33d29e69c7795c9d03c7b5d5f8b57e5a66c1821
Xenthio <thegooster88@gmail.com>
2024-01-03 14:00:08 +1100

    Torch

---
+++ code/Player/TorchController.cs
@@ -0,0 +1,22 @@
+﻿namespace FPSKit;
+
+public class TorchController : Component
+{
+	[Property] public SpotLight TorchLight { get; set; }
+	float distance = 0;
+	protected override void OnFixedUpdate()
+	{
+		base.OnFixedUpdate();
+		if ( Input.Pressed( "Flashlight" ) )
+		{
+			TorchLight.Enabled = !TorchLight.Enabled;
+		}
+		if ( TorchLight.Enabled )
+		{
+			var tr = Scene.Trace.Ray( TorchLight.GameObject.Parent.Transform.Position, TorchLight.GameObject.Parent.Transform.Position + (TorchLight.GameObject.Transform.Rotation.Forward * 24) ).WithoutTags( "player" ).Run();
+
+			distance = distance.LerpTo( tr.Distance, Time.Delta * 16f );
+			TorchLight.Transform.LocalPosition = -Vector3.Forward * (4 - distance);
+		}
+	}
+}
