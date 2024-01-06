e5371b352c0121677faa0ac688503c03d687b3e3
Xenthio <thegooster88@gmail.com>
2024-01-06 11:28:50 +1100

    lower phys substeps

--- code/Game/NetworkManager.cs
+++ code/Game/NetworkManager.cs
@@ -36,7 +36,7 @@ public sealed class NetworkManager : Component, Component.INetworkListener
 	/// </summary>
 	public void OnActive( Connection channel )
 	{
-		GameManager.ActiveScene.PhysicsWorld.SubSteps = 4;
+		GameManager.ActiveScene.PhysicsWorld.SubSteps = 2;
 		Log.Info( $"Player '{channel.DisplayName}' has joined the game" );
 
 		if ( PlayerPrefab is null )
